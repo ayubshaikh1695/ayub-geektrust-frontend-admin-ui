d6704a63bc0f139628932c102222ab161f59e3ec
ayubshaikh1695 <ayubshaikh1695@gmail.com>
2023-01-02 05:09:39 +0530

    added default exports of all components

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+export { default as Button } from "./button/button.component";
+export { default as Checkbox } from "./checkbox/checkbox.component";
+export { default as IconButton } from "./icon-button/icon-button.component";
+export { default as Radio } from "./radio/radio.component";
+export { default as Table } from "./table/table.component";
+export { default as TableBody } from "./table-body/table-body.component";
+export { default as TableCell } from "./table-cell/table-cell.component";
+export { default as TableHead } from "./table-head/table-head.component";
+export { default as TableRow } from "./table-row/table-row.component";
+export { default as TextField } from "./text-field/text-field.component";
+export { default as Typography } from "./typography/typography.component";
